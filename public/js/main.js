cbfda2414c3e86175a4d3cd41015f434753a6a5f
Caleb Phillips <csphillips5@gmail.com>
2022-09-03 14:57:20 -0400

    implemented frontend sort functionality of movies list

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,8 @@ const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('span.not')
 const todoComplete = document.querySelectorAll('span.completed')
 
+const sortSelector = document.querySelector('#sortID').addEventListener('change', displaySortedMovies)
+
 Array.from(deleteBtn).forEach((el)=>{
     el.addEventListener('click', deleteTodo)
 })
@@ -67,4 +69,29 @@ async function markIncomplete(){
     }catch(err){
         console.log(err)
     }
+}
+
+function displaySortedMovies(e) {
+    console.log(e.target.value);
+    const sortedBy = e.target.value
+    // create document fragment
+    const domFragment = document.createDocumentFragment()
+    // access the list of movies / ul
+    const movieList = document.querySelector('ul')
+    // access all the individual movies / li & store in an array
+    const movies = Array.from(document.querySelectorAll('.todoItem'))
+
+    movies // sort li elems according to the target value / option selected
+      .sort((a, b) => {
+          // for now only two options implemented, title or genre
+          const option = sortedBy === 'title' ? 0 : 1
+          const c = a.children[option].innerText,
+                d = b.children[option].innerText
+          return c.localeCompare(d);
+      }) // add elements to fragment
+      .forEach(movie => {
+        domFragment.appendChild(movie)
+      })
+    // add fragment to movie list / ul
+    movieList.appendChild(domFragment)
 }
